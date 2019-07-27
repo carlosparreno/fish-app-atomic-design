6912b4d2e3482bed89094cacaca64f160f27b5fb
Carlos Parreño <carlos.parreno@hotmail.es>
2019-07-27 18:22:50 +0100

    :new: Add Label component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import Label from "./atoms/Label";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <Label>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </Label>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/atoms/Label.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Label(props) {
+  return <label className="f-label">{props.children}</label>;
+}
+
+export default Label;
