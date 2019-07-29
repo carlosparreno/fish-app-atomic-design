f3d990d77a4981330a98b1820fe58216f9ec3ac0
Carlos Parreño <carlos.parreno@hotmail.es>
2019-07-29 22:03:46 +0100

    :art: Improve styling

--- src/atoms/Icon.js
+++ src/atoms/Icon.js
@@ -3,6 +3,7 @@ import React from 'react';
 function Icon(props) {
   return (
     <span
+      className="f-icon"
       role="img"
       aria-label={props.ariaLabel}
       onMouseEnter={props.onMouseEnter}
